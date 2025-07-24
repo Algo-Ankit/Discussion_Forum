543c2298d45a29f1a9ce3e5dac115c252942b94a
Ankit Anand Singh <ankitanandat@gmail.com>
2025-07-25 01:47:07 +0530

    Cleaned requirements and updated Render config

--- devhub/settings.py
+++ devhub/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('DJANGO_SECRET_KEY', default='your_real_django_secret_key_here')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']  # or your Render domain
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="").split(",")
 
 
 # Application definition
