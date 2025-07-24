6a429444f27e3dec04e56d396e8d7cd8be8ba14f
Ankit Anand Singh <ankitanandat@gmail.com>
2025-07-25 02:12:35 +0530

    Fix ALLOWED_HOSTS and settings for Render deployment

--- devhub/settings.py
+++ devhub/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="").split(",")
+ALLOWED_HOSTS = ['devhub-28rg.onrender.com']
+
+
 
 
 # Application definition
